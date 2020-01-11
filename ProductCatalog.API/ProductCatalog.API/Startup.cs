e660bec13b004a2e7c35011fdf6dec2985769e85
Aslı Öztürk <ozturkaslii@gmail.com>
2020-01-11 03:57:25 +0300

    mediatr library added to the project

--- ProductCatalog.API/ProductCatalog.API/Startup.cs
+++ ProductCatalog.API/ProductCatalog.API/Startup.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -31,6 +32,8 @@ namespace ProductCatalog.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
             services.AddControllers();
 
             services.AddDbContext<ProductCatalogContext>(options =>
